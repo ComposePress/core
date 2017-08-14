71b7efab67a5a33af449ca2b3c94f8152d3497a0
Derrick Hammer <derrick@derrickhammer.com>
2017-08-14 10:34:45 -0400

    *Move set_component_parents and get_components to componentabstract

--- src/ComponentAbstract.php
+++ src/ComponentAbstract.php
@@ -71,4 +71,47 @@ abstract class ComponentAbstract extends BaseObjectAbstract {
 		return null !== $this->parent;
 	}
 
+
+	/**
+	 * @param $components
+	 */
+	protected function set_component_parents( $components ) {
+		/** @var ComponentAbstract $component */
+		foreach ( $components as $component ) {
+			$component->parent = $this;
+		}
+	}
+
+	/**
+	 * @return array|\ReflectionProperty[]
+	 */
+	protected function get_components() {
+		$components = ( new \ReflectionClass( $this ) )->getProperties();
+		$components = array_filter(
+			$components,
+			/**
+			 * @param \ReflectionProperty $component
+			 *
+			 * @return bool
+			 */
+			function ( $component ) {
+				$getter = 'get_' . $component->name;
+
+				return method_exists( $this, $getter ) && ( new \ReflectionMethod( $this, $getter ) )->isPublic() && $this->$getter() instanceof ComponentAbstract;
+			} );
+		$components = array_map(
+		/**
+		 * @param \ReflectionProperty $component
+		 *
+		 * @return ComponentAbstract
+		 */
+			function ( $component ) {
+				$getter = 'get_' . $component->name;
+
+				return $this->$getter();
+			}, $components );
+
+		return $components;
+	}
+
 }
