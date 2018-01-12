6977797917ae23095ab573b0ef2b58e9279fcdae
Derrick Hammer <derrick@derrickhammer.com>
2018-01-12 11:12:18 -0500

    *Add phpdocs

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,20 +1,39 @@
 <?php
+/**
+ *
+ */
 define( 'ABSPATH', '/tmp/wordpress/' );
 // Initialize composer
 require_once dirname( __DIR__ ) . '/vendor/autoload.php';
 if ( ! class_exists( '\PHPUnit_Framework_TestCase' ) && class_exists( '\PHPUnit\Framework\TestCase' ) ) {
+	/**
+	 * Class PHPUnit_Framework_TestCase
+	 */
 	class_alias( '\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase' );
 }
 WP_Mock::bootstrap();
 require_once ABSPATH . 'wp-includes/class-wp-error.php';
+/**
+ * @param $string
+ *
+ * @return string
+ */
 function trailingslashit( $string ) {
 	return untrailingslashit( $string ) . '/';
 }
 
+/**
+ * @param $string
+ *
+ * @return string
+ */
 function untrailingslashit( $string ) {
 	return rtrim( $string, '/\\' );
 }
 
+/**
+ *
+ */
 function wp_set_lang_dir() {
 	if ( ! defined( 'WP_LANG_DIR' ) ) {
 		if ( file_exists( WP_CONTENT_DIR . '/languages' ) && @is_dir( WP_CONTENT_DIR . '/languages' ) || ! @is_dir( ABSPATH . WPINC . '/languages' ) ) {
@@ -47,10 +66,20 @@ function wp_set_lang_dir() {
 	}
 }
 
+/**
+ * @param $thing
+ *
+ * @return bool
+ */
 function is_wp_error( $thing ) {
 	return ( $thing instanceof WP_Error );
 }
 
+/**
+ * @param     $tag
+ * @param     $function_to_remove
+ * @param int $priority
+ */
 function remove_filter( $tag, $function_to_remove, $priority = 10 ) {
 
 }
