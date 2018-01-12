d7c27a4e352ea0ce2410f5ef0625109f40ac54ec
Derrick Hammer <derrick@derrickhammer.com>
2018-01-12 13:03:30 -0500

    *Fall back to setExpectedException

--- tests/ComponentTest.php
+++ tests/ComponentTest.php
@@ -5,7 +5,11 @@ use pcfreak30\ComposePress\ComponentMock;
 
 class ComponentTest extends PHPUnit_Framework_TestCase {
 	public function test_child_component_null() {
-		$this->expectException( '\Exception' );
+		if ( method_exists( $this, 'expectException' ) ) {
+			$this->expectException( '\Exception' );
+		} else {
+			$this->setExpectedException( '\Exception' );
+		}
 		$component = new ComponentMock();
 		$this->assertNull( $component->child->parent );
 		$this->assertInstanceOf( '\pcfreak30\ComposePress\Abstracts\Component', $component->child->plugin );
